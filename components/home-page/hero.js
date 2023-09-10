df1a4b4281f63a41cce5946aa62291755cd565e9
Sherry90 <hhlsdkm@gmail.com>
2023-09-10 19:46:59 +0900

    formatted

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <section className={classes.hero}>
       <div className={classes.image}>
-          <Image src="/images/site/mac.png" alt="" width={680} height={756} />
+        <Image src="/images/site/mac.png" alt="" width={680} height={756} />
       </div>
       <h1>Hi, I'm Mac</h1>
       <p>blog</p>
