35a4416e39dd440e74d86b1c154469d18c693e4f
Sherry90 <hhlsdkm@gmail.com>
2023-09-10 19:44:39 +0900

    add hero components

---
+++ components/home-page/hero.js
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+import classes from "./hero.module.css";
+
+const Hero = () => {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+          <Image src="/images/site/mac.png" alt="" width={680} height={756} />
+      </div>
+      <h1>Hi, I'm Mac</h1>
+      <p>blog</p>
+    </section>
+  );
+};
+
+export default Hero;
