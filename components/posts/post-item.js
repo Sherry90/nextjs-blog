afc6f2678bc76b8f4f7f6f5a8b41d93af746ca21
Sherry90 <hhlsdkm@gmail.com>
2023-09-24 17:35:04 +0900

    layout response

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -13,19 +13,25 @@ const PostItem = (props) => {
   });
 
   const imagePath = `/images/posts/${slug}/${image}`;
-  const linkPath = `/posts/${slug}`
+  const linkPath = `/posts/${slug}`;
 
   return (
     <li className={classes.post}>
       <Link href={linkPath}>
-          <div className={classes.image}>
-            <Image src={imagePath} alt={title} width={300} height={200} />
-          </div>
-          <div className={classes.content}>
-            <h3>{title}</h3>
-            <time>{formattedDate}</time>
-            <p>{excerpt}</p>
-          </div>
+        <div className={classes.image}>
+          <Image
+            src={imagePath}
+            alt={title}
+            width={300}
+            height={200}
+            layout="response"
+          />
+        </div>
+        <div className={classes.content}>
+          <h3>{title}</h3>
+          <time>{formattedDate}</time>
+          <p>{excerpt}</p>
+        </div>
       </Link>
     </li>
   );
