eb35f9ed70a28287e0a1f7d4cbcb1ee5f4ffe3a7
Sherry90 <hhlsdkm@gmail.com>
2023-09-24 17:03:18 +0900

    rendering FeaturedPosts with dummy-data

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,44 @@
 import { Fragment } from "react";
 
 import Hero from "../components/home-page/hero";
+import FeaturedPosts from "../components/home-page/featured-posts";
+
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-nextjs1",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS Fullstack course",
+    date: "2023-09-24",
+  },
+  {
+    slug: "getting-started-with-nextjs2",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS Fullstack course",
+    date: "2023-09-24",
+  },
+  {
+    slug: "getting-started-with-nextjs3",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS Fullstack course",
+    date: "2023-09-24",
+  },
+  {
+    slug: "getting-started-with-nextjs4",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS Fullstack course",
+    date: "2023-09-24",
+  },
+];
 
 const HomePage = () => {
   return (
     <Fragment>
       <Hero />
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </Fragment>
   );
 };
