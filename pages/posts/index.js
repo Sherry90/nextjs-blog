a51a3475ff0b6a741b908738f51a46cc402d09ca
Sherry90 <hhlsdkm@gmail.com>
2023-09-26 18:19:26 +0900

    import AllPosts and DUMMY_POSTS

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,38 @@
-const AllPostPage = () => {};
+import AllPosts from "../../components/posts/all-posts";
+
+const DUMMY_POSTS = [
+    {
+        slug: "getting-started-with-nextjs",
+        title: "Getting Started with NextJS",
+        image: "getting-started-nextjs.png",
+        excerpt: "NextJS Fullstack course",
+        date: "2023-09-24",
+    },
+    {
+        slug: "getting-started-with-nextjs2",
+        title: "Getting Started with NextJS",
+        image: "getting-started-nextjs.png",
+        excerpt: "NextJS Fullstack course",
+        date: "2023-09-24",
+    },
+    {
+        slug: "getting-started-with-nextjs3",
+        title: "Getting Started with NextJS",
+        image: "getting-started-nextjs.png",
+        excerpt: "NextJS Fullstack course",
+        date: "2023-09-24",
+    },
+    {
+        slug: "getting-started-with-nextjs4",
+        title: "Getting Started with NextJS",
+        image: "getting-started-nextjs.png",
+        excerpt: "NextJS Fullstack course",
+        date: "2023-09-24",
+    },
+];
+
+const AllPostPage = () => {
+    return <AllPosts posts={DUMMY_POSTS} />
+};
 
 export default AllPostPage;
