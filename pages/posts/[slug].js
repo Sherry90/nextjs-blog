88e02c6602570a9fa93f4721f3d6dbf60689ab13
Sherry90 <hhlsdkm@gmail.com>
2023-10-01 18:40:37 +0900

    render PostContent real data

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,32 @@
 import PostContent from "../../components/posts/post-detail/post-content";
+import { getPostData, getPostsFiles } from "../../lib/posts-util";
 
-const PostDetailPage = () => {
-  return <PostContent />;
+const PostDetailPage = (props) => {
+  return <PostContent post={props.posts} />;
+};
+
+export const getStaticProps = (context) => {
+  const slug = context.params.slug;
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      posts: postData,
+    },
+    revalidate: 600,
+  };
+};
+
+export const getStaticPaths = () => {
+  const postFilenames = getPostsFiles();
+
+  const slugs = postFilenames.map((fileName) => fileName.replace(/\.md$/, ""));
+
+  return {
+    paths: slugs.map((slug) => ({ params: { slug } })),
+    fallback: false,
+  };
 };
 
 export default PostDetailPage;
