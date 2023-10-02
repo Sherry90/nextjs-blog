ebb39d1c66eac982eb9458d3ccad2609bdec52ee
Sherry90 <hhlsdkm@gmail.com>
2023-10-02 17:51:30 +0900

    ReactMarkdown render from customRenderers

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -2,15 +2,53 @@ import ReactMarkdown from "react-markdown";
 
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
+import Image from "next/image";
 
 const PostContent = (props) => {
   const { post } = props;
+
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = {
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+        const altText = typeof image.properties.alt === 'string' ? image.properties.alt : '';
+        
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={altText}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+
+    // code(code) {
+    //   const { className, children } = code;
+    //   const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
+    //   return (
+    //     <SyntaxHighlighter
+    //       style={atomDark}
+    //       language={language}
+    //       children={children}
+    //     />
+    //   );
+    // },
+  };
 
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 };
