a67218fd934f4c37e382fbe627c64850de5a06ea
Sherry90 <hhlsdkm@gmail.com>
2023-10-05 17:37:45 +0900

    rendering contact-from.js

--- pages/contact.js
+++ pages/contact.js
@@ -1,3 +1,7 @@
-const ContactPage = () => {};
+import ContactFrom from "../components/contact/contact-from";
+
+const ContactPage = () => {
+  return <ContactFrom />
+};
 
 export default ContactPage;
