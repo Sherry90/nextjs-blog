da87cbc0ca5c3239a9e1bc1e0379f44885aa8881
Sherry90 <hhlsdkm@gmail.com>
2023-10-14 17:38:11 +0900

    add Head homepage

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,20 @@
 import { Fragment } from "react";
+import Head from "next/head";
 
-import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
+import Hero from "../components/home-page/hero";
 import { getFeaturedPosts } from "../lib/posts-util";
 
 const HomePage = (props) => {
   return (
     <Fragment>
+      <Head>
+        <title>Sherry's Blog</title>
+        <meta
+          name="description"
+          content="I post about programming and web develop"
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </Fragment>
