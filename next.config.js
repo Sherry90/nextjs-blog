3ea41f1a9c569bc2b19f4be968b73dac87dc7e78
Sherry90 <hhlsdkm@gmail.com>
2023-10-15 17:56:35 +0900

    del env

--- next.config.js
+++ next.config.js
@@ -1,35 +1,22 @@
 const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-require("dotenv").config();
-
-const {
-  DB_USERNAME,
-  DB_PASSWORD,
-  DB_CLUSTERNAME,
-  DB_NAME_DEV,
-  DB_NAME,
-  MESSAGE_COLLECTION,
-} = process.env;
-
 module.exports = (phase) => {
   if(phase === PHASE_DEVELOPMENT_SERVER) {
     return {
       env: {
-        mongodb_username: DB_USERNAME,
-        mongodb_password: DB_PASSWORD,
-        mongodb_clustername: DB_CLUSTERNAME,
-        mongodb_database: DB_NAME_DEV,
-        mongodb_messagecollection: MESSAGE_COLLECTION,
+        mongodb_username: "sherry",
+        mongodb_password: "MeMfVpuMWo3LdqiQ",
+        mongodb_clustername: "cluster0",
+        mongodb_database: "my-site-dev",
       },
     }
   }
   return {
     env: {
-      mongodb_username: DB_USERNAME,
-      mongodb_password: DB_PASSWORD,
-      mongodb_clustername: DB_CLUSTERNAME,
-      mongodb_database: DB_NAME,
-      mongodb_messagecollection: MESSAGE_COLLECTION,
+      mongodb_username: "sherry",
+      mongodb_password: "MeMfVpuMWo3LdqiQ",
+      mongodb_clustername: "cluster0",
+      mongodb_database: "my-site",
     },
   };
 };
